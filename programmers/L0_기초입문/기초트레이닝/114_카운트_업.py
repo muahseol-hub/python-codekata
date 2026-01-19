33a266173a412dc1c8498fadcca21d0831f48a5e
muahseol-hub <muahseol@gmail.com>
2026-01-19 20:46:56 +0900

    [프로그래머스] L0 - 카운트 업 (#114)

---
+++ programmers/L0_기초입문/기초트레이닝/114_카운트_업.py
@@ -0,0 +1,10 @@
+# 카운트 업
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181920
+# 알고리즘: 반복문
+# 작성자: 설무아
+# 작성일: 2026. 01. 19. 20:46:53
+
+def solution(start_num, end_num):
+    answer = [i for i in range(start_num, end_num+1)] #dd
+    return answer
